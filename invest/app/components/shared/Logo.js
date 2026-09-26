7ac951662fe28879b2715303d4e56833251db2b1
claudiorogb <claudiorogb@gmail.com>
2026-09-26 14:29:34 -0300

    Corrigir logotipo AXIVA no site comercial

--- invest/app/components/shared/Logo.js
+++ invest/app/components/shared/Logo.js
@@ -1,5 +1,4 @@
 import React from 'react';
-const AXIVA_LOGO='https://drive.google.com/uc?export=view&id=1Oh-uMvTQW7XjjzTfHQQVKbcgkW8d23lv';
-export default function Logo({className=''}){
-  return React.createElement('img',{src:AXIVA_LOGO,alt:'AXIVA',className:`h-9 w-auto object-contain ${className}`});
+export default function Logo({className=''}) {
+  return React.createElement('img',{src:'/invest/axiva-logo.webp',alt:'AXIVA',className:`h-10 w-auto object-contain ${className}`});
 }
